4e77d74d44e70d9af3c13b98aad84acc0e3c98c8
Simon Chester <schester@vertical-leap.uk>
2023-12-13 13:48:07 +0000

    Working doc creator

---
+++ ChartInWordDocDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+
+namespace ChartInWordDocDemo;
+
+public class Program
+{
+    public static void Main()
+    {
+        using var memoryStream = new MemoryStream();
+        CreateDocument(memoryStream);
+        memoryStream.Position = 0;
+        memoryStream.CopyTo(File.Create("Output.docx"));
+    }
+
+    public static void CreateDocument(Stream outputStream)
+    {
+        using var document = WordprocessingDocument.CreateFromTemplate("Template.dotx");
+        //ChartCreator.CreateChart(document.MainDocumentPart);
+        document.Clone(outputStream);
+    }
+
+}
